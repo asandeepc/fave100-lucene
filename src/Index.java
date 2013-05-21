eeae6224f063b4cb49dafe9774d2076a98eed408
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-05-21 10:33:39 -0400

    Updated to Lucene 4.3

--- src/Index.java
+++ src/Index.java
@@ -21,29 +21,29 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
-
 public class Index {
 
-	public static void main(final String [] args) {
+	public static void main(final String[] args) {
 		// Analyzer with no stopwords
 		final File file = new File("/path/to/file");
-		final WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer(Version.LUCENE_42);
+		final WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer(Version.LUCENE_43);
 		// Delete all old indexes
 		final String[] myFiles = file.list();
-		for(int i = 0; i < myFiles.length; i++) {
+		for (int i = 0; i < myFiles.length; i++) {
 			final File subFile = new File(file, myFiles[i]);
 			subFile.delete();
 		}
 		Directory index = null;
 		try {
 			index = FSDirectory.open(file);
-		} catch (final IOException e1) {
+		}
+		catch (final IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 
 		final LogDocMergePolicy mergePolicy = new LogDocMergePolicy();
-		final IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_42, analyzer);
+		final IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_43, analyzer);
 		config.setMergePolicy(mergePolicy);
 
 		Connection connection = null;
@@ -57,37 +57,40 @@ public class Index {
 		    connection = DriverManager.getConnection(url, user, password);
 			final String statement = "SELECT * FROM table;";
 			final Statement stmt = connection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
-		              java.sql.ResultSet.CONCUR_READ_ONLY);
+					java.sql.ResultSet.CONCUR_READ_ONLY);
 			stmt.setFetchSize(100000);
 			connection.setAutoCommit(false);
 			final ResultSet results = stmt.executeQuery(statement);
 			try {
 				final IndexWriter w = new IndexWriter(index, config);
-				while(results.next()) {
+				while (results.next()) {
 					final Document document = new Document();
 					document.add(new Field("id", String.valueOf(results.getInt("id")), idType()));
 					document.add(new StoredField("song", results.getString("song")));
 					document.add(new StoredField("artist", results.getString("artist")));
 					document.add(new NumericDocValuesField("rank", results.getInt("rank")));
 
-					// TODO: See if it is possible to use correct filters with Lucene so we don't need extra fields
-					final Field searchField = new Field("searchable_song_artist", results.getString("searchable_song")+" "+results.getString("searchable_artist"), indexType());
+					final Field searchField = new Field("searchable_song_artist", results.getString("searchable_song") + " " + results.getString("searchable_artist"), indexType());
 					searchField.setBoost(results.getInt("rank"));
 					document.add(searchField);
 					w.addDocument(document);
 				}
 				w.close();
-			} catch (final IOException e) {
+			}
+			catch (final IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		} catch (final SQLException ignore) {
+		}
+		catch (final SQLException ignore) {
 			ignore.printStackTrace();
-		} finally {
+		}
+		finally {
 			if (connection != null) {
 				try {
 					connection.close();
-				} catch (final SQLException ignore) {
+				}
+				catch (final SQLException ignore) {
 					ignore.printStackTrace();
 				}
 			}
@@ -97,18 +100,18 @@ public class Index {
 	}
 
 	private static FieldType idType() {
-        final FieldType idType = new FieldType();
-        idType.setIndexed(true);
-        idType.setStored(true);
-        return idType;
+		final FieldType idType = new FieldType();
+		idType.setIndexed(true);
+		idType.setStored(true);
+		return idType;
 	}
 
 	private static FieldType indexType() {
-        final FieldType indexType = new FieldType();
-        indexType.setIndexed(true);
-        indexType.setStored(false);
-        indexType.setTokenized(true);
-        return indexType;
+		final FieldType indexType = new FieldType();
+		indexType.setIndexed(true);
+		indexType.setStored(false);
+		indexType.setTokenized(true);
+		return indexType;
 	}
 
 }
