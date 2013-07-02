3d7f29b63995e884a4dbf4ca8fcf34c036c14d8c
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-07-02 15:41:03 -0400

    Better time tracking

--- src/Index.java
+++ src/Index.java
@@ -5,14 +5,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.Date;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.util.CharArraySet;
+
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.FieldType;
-import org.apache.lucene.document.NumericDocValuesField;
 import org.apache.lucene.document.StoredField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -24,6 +22,7 @@ import org.apache.lucene.util.Version;
 public class Index {
 
 	public static void main(final String[] args) {
+		final long startTime = new Date().getTime();
 		// Analyzer with no stopwords
 		final File file = new File("/path/to/file");
 		final StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_43, new CharArraySet(Version.LUCENE_43, new ArrayList<>(), true));
@@ -33,6 +32,8 @@ public class Index {
 			final File subFile = new File(file, myFiles[i]);
 			subFile.delete();
 		}
+		final long deleteTime = new Date().getTime();
+		System.out.println("Time to delete old index: " + (deleteTime - startTime));
 		Directory index = null;
 		try {
 			index = FSDirectory.open(file);
@@ -48,9 +49,9 @@ public class Index {
 
 		int count = 0;
 		Connection connection = null;
+		long sqlTime = 0;
 		try {
-			System.out.println("SQL started");
-			System.out.println("Building index...");
+			System.out.println("Connecting to SQL...");
 			// Make connection
 			final String url = "url";
 		    final String user = "root";
@@ -63,43 +64,65 @@ public class Index {
 			stmt.setFetchSize(100000);
 			connection.setAutoCommit(false);
 			final ResultSet results = stmt.executeQuery(statement);
+			sqlTime = new Date().getTime();
+			System.out.println("Time to execute SQL: " + (sqlTime - deleteTime));
+			System.out.println("Building index...");
 			try {
 				final IndexWriter w = new IndexWriter(index, config);
+				// Create fields once only, to avoid GC				
+				final Field idField = new Field("id", "", idType());
+				final StoredField songField = new StoredField("song", "");
+				final StoredField artistField = new StoredField("artist", "");
+				//				final NumericDocValuesField rankField = new NumericDocValuesField("rank", 0);
+				final Field searchField = new Field("searchable_song_artist", "", indexType());
+
 				while (results.next()) {
 					count++;
-					final Document document = new Document();
-					document.add(new Field("id", String.valueOf(results.getInt("id")), idType()));
-					document.add(new StoredField("song", results.getString("song")));
-					document.add(new StoredField("artist", results.getString("artist")));
-					document.add(new NumericDocValuesField("rank", results.getInt("rank")));
 
-					final Field searchField = new Field("searchable_song_artist", results.getString("searchable_song") + " " + results.getString("searchable_artist"), indexType());
+					final Document document = new Document();
+					idField.setStringValue(String.valueOf(results.getInt("id")));
+					songField.setStringValue(results.getString("song"));
+					artistField.setStringValue(results.getString("artist"));
+					//					rankField.setLongValue(results.getInt("rank"));
+
+					document.add(idField);
+					document.add(songField);
+					document.add(artistField);
+					//					document.add(rankField);
+					searchField.setStringValue(results.getString("searchable_song") + " " + results.getString("searchable_artist"));
 					searchField.setBoost(results.getInt("rank"));
 					document.add(searchField);
 					w.addDocument(document);
+
 				}
+				w.forceMerge(1);
 				w.close();
 			}
 			catch (final IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println(count);
 		}
-		catch (final SQLException ignore) {
-			ignore.printStackTrace();
+		catch (final SQLException e) {
+			e.printStackTrace();
 		}
 		finally {
 			if (connection != null) {
 				try {
 					connection.close();
 				}
-				catch (final SQLException ignore) {
-					ignore.printStackTrace();
+				catch (final SQLException e) {
+					e.printStackTrace();
 				}
 			}
 			System.out.println("Index built: " + count + " entries");
 		}
+		final long endTime = new Date().getTime();
+		System.out.println("Time to build index: " + (endTime - sqlTime));
+		final int totalMilli = (int)(endTime - startTime);
+		final int minutes = totalMilli / 1000 / 60;
+		final int seconds = (totalMilli / 1000) % 60;
+		final int milli = totalMilli % 1000;
+		System.out.println("Total time: " + minutes + "m " + seconds + "s " + milli + "ms ");
 
 	}
 
